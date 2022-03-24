d1e2e6cf84c7c96ca005f26011a696a74a46cdd0
Joey Zeng <joey>
2022-03-24 12:14:25 +0800

    fix

--- conet/http_client.h
+++ conet/http_client.h
@@ -20,25 +20,29 @@ class HttpClient
 public:
     HttpClient(Executor &ex) :
         executor_(ex),
-        resolver_(ex),
         stream_(ex)
     {
     }
 
     boost::asio::awaitable<result<std::string>> co_get(const std::string &url)
     {
-        RESULT_CO_CHECK(parser_.parse(url));
+        UrlParser parser;
+        RESULT_CO_CHECK(parser.parse(url));
 
-        auto results = co_await resolver_.async_resolve(parser_.host(), parser_.service(), boost::asio::use_awaitable);
+        boost::asio::ip::tcp::resolver resolver(stream_.get_executor());
+        auto results = co_await resolver.async_resolve(parser.host(), parser.service(), boost::asio::use_awaitable);
         co_await stream_.async_connect(results, boost::asio::use_awaitable);
 
-        req_.method(boost::beast::http::verb::get);
-        req_.target(std::string(parser_.path()));
-        req_.version(11);
-        req_.set(boost::beast::http::field::host, std::string(parser_.host()));
-        req_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-        co_await boost::beast::http::async_write(stream_, req_, boost::asio::use_awaitable);
-        co_await boost::beast::http::async_read(stream_, rsp_buffer_, rsp_, boost::asio::use_awaitable);
+        boost::beast::http::request<boost::beast::http::string_body> req;
+        boost::beast::flat_buffer rsp_buffer;
+        boost::beast::http::response<boost::beast::http::string_body> rsp;
+        req.method(boost::beast::http::verb::get);
+        req.target(std::string(parser.path()));
+        req.version(11);
+        req.set(boost::beast::http::field::host, std::string(parser.host()));
+        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        co_await boost::beast::http::async_write(stream_, req, boost::asio::use_awaitable);
+        co_await boost::beast::http::async_read(stream_, rsp_buffer, rsp, boost::asio::use_awaitable);
 
         boost::system::error_code ec;
         stream_.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
@@ -50,17 +54,12 @@ public:
             co_return error_code;
         }
 
-        co_return rsp_.body();
+        co_return rsp.body();
     }
 
 private:
     Executor& executor_;
-    UrlParser parser_;
-    boost::asio::ip::tcp::resolver resolver_;
     boost::beast::tcp_stream stream_;
-    boost::beast::http::request<boost::beast::http::string_body> req_;
-    boost::beast::flat_buffer rsp_buffer_;
-    boost::beast::http::response<boost::beast::http::string_body> rsp_;
 };
 
 } // namespace conet
